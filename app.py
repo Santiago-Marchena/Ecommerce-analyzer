a01e32eb5e10b72cbfdef4bbedbcf11938cfefd8
Santiago Marchena <marchena.zafra@gmail.com>
2026-05-23 18:09:49 -0500

    Fix Railway port

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from models import Product
 from services import analyze_product
-
+import os
 app = Flask(__name__)
 
 
@@ -30,4 +30,5 @@ def analyze():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
